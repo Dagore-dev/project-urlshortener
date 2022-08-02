e326b1e923af18891571ac626b568e8f6073a90c
Dagore <david.gomez.redondo.dev@gmail.com>
2022-08-02 18:57:31 +0200

    URL validation

--- utils/isValidUrl.js
+++ utils/isValidUrl.js
@@ -1,4 +1,17 @@
+const dns = require('dns/promises')
+
 function isValidUrl (url) {
+  try {
+    const newURL = new URL(url)
+    dns.lookup(newURL.hostname, {}, (err) => {
+      console.log('on dns lookup', err)
+      throw err
+    })
+  } catch (err) {
+    console.log('on new URL', err)
+    return false
+  }
+
   return true
 }
 
